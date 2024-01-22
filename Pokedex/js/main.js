1c79274c8685d8847c3774024cb504057f9b7dbb
Rafael Pinheiro <82719410+radarkness@users.noreply.github.com>
2024-01-22 10:40:46 -0300

    atualização 22.01.2024

--- Pokedex/js/main.js
+++ Pokedex/js/main.js
@@ -1,3 +1,79 @@
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function (response) {
+        console.log(response)
+    })
+    .catch(function (error) {
+        console.error(error)
+    })
+    .finally(function(){
+        console.log('Requisição concluída')
+    })
+
+const x = 2000;
+console.log(x);
 
 
-console.log('sucesso!')
+
+
+
+
+
+
+
+
+/*
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
+*/
+
+
+
