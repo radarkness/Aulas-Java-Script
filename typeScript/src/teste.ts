d19473393e5218fdd4bbc7ce8c6f414237845351
Rafael Pinheiro <82719410+radarkness@users.noreply.github.com>
2024-02-09 09:36:45 -0300

    09-02-2024

--- typeScript/src/teste.ts
+++ typeScript/src/teste.ts
@@ -1,5 +1,11 @@
-function apiVersion(version: string){
+/*function apiVersion(version: string){
     return (target: any) => {
         Object.assign(target.prototype, {__version: version});
     }
 }
+
+@apiVersion("07.97")
+class api {
+    const api = new api();
+    console.log(api.__version);
+}*/
